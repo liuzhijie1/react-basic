0d335b11abd52cf332e0934904650b7e65daf666
liuzhijie1 <572890972@qq.com>
2023-04-10 22:29:38 +0800

    昨天手打

--- stepone.jsx
+++ stepone.jsx
@@ -113,19 +113,33 @@ function commitWork(fiber) {
   if (!fiber) {
     return
   }
-  const domParent = fiber.parent.dom
+  // const domParent = fiber.parent.dom
+  let domParentFiber = fiber.parent
+  while(!domParentFiber.dom) {
+    domParentFiber = domParentFiber.parent
+  }
+  const domParent = domParentFiber.dom;
 
   if (fiber.effectTag === 'PLACEMENT' && fiber.dom !== null) {
     domParent.appendChild(fiber.dom)
   } else if (fiber.effectTag === 'UPDATE' && fiber.dom !== null) {
     updateDom(fiber.dom, fiber.alternate.props, fiber.props)
   } else if (fiber.effectTag === 'DELETION') {
-    domParent.removeChild(fiber.dom)
+    // domParent.removeChild(fiber.dom)
+    commitDeletion(fiber, domParent)
   }
 
 
   commitWork(fiber.child)
   commitWork(fiber.sibling)
+}
+
+function commitDeletion(fiber, domParent) {
+  if (fiber.dom) {
+    domParent.removeChild(fiber.dom)
+  } else {
+    commitDeletion(fiber.child, domParent)
+  }
 }
 
 function render(element, container) {
@@ -165,16 +179,24 @@ requestIdleCallback(workLoop)
 
 function performUnitOfWork(fiber) {
   // TODO 
-  // TODO add dom node
-  if (!fiber.dom) {
-    fiber.dom = createDom(fiber)
+
+  const isFunctionComponent = fiber.type instanceof Function
+  if (isFunctionComponent) {
+    updateFunctionComponent(fiber)
+  } else {
+    updateHostComponent(fiber)
   }
+
+  // TODO add dom node
+  // if (!fiber.dom) {
+  //   fiber.dom = createDom(fiber)
+  // }
   // if (fiber.parent) {
   //   fiber.parent.dom.appendChild(fiber.dom)
   // }
   // TODO create new fibers
-  const elements = fiber.props.children
-  reconcileChildren(fiber, elements);
+  // const elements = fiber.props.children
+  // reconcileChildren(fiber, elements);
 
   // TODO return next unit of work
 
@@ -188,6 +210,19 @@ function performUnitOfWork(fiber) {
     }
     nextFiber = nextFiber.parent
   }
+}
+
+function updateFunctionComponent(fiber) {
+  // TODO
+  const children = [fiber.type(fiber.props)]
+  reconcileChildren(fiber, children)
+}
+
+function updateHostComponent(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+  reconcileChildren(fiber, fiber.props.children)
 }
 
 function reconcileChildren(wipFiber, elements) {
@@ -259,9 +294,24 @@ const Didact = {
 }
 
 /** @jsx Didact.createElement */
-const element = Didact.createElement(
-  "div",
-  { id: 'foo' },
-  Didact.createElement('a', null, 'bar'),
-  Didact.createElement('b')
-)
+// const element = Didact.createElement(
+//   "div",
+//   { id: 'foo' },
+//   Didact.createElement('a', null, 'bar'),
+//   Didact.createElement('b')
+// )
+/** @jsx Didact.createElement */
+function App(props) {
+  return <h1>Hi {props.name}</h1>
+}
+const element = <App name="foo" />
+const container = document.getElementById('root')
+Didact.render(element, container)
+/**
+ * it will transform the jsx to js
+ * function App(props) {
+ *  return Didact.createElement('h1', null, 'hi ', props.name) 
+ * }
+ * 
+ * const element = Didact.createElement(App, {name: 'foo'})
+ */
